1a2a35b1413b766486975aeb8b3dc5da56c57c26
Mohammed-Waheed27 <your-email@example.com>
2025-12-16 16:10:10 +0200

    design remake

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3619602749"
+    serviceWorkerVersion: "3851173273"
   }
 });
